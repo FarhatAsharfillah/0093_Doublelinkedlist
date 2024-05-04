9c046ef76966acfc283bce322dfb6fa4963b8a3d
FarhatAsharfillah <farhatasharfillah03@gmail.com>
2024-05-04 09:21:44 +0700

    commit 5 input while dalam else

--- 0093_Doublelinkedlist/0093_Doublelinkedlist.cpp
+++ 0093_Doublelinkedlist/0093_Doublelinkedlist.cpp
@@ -25,12 +25,21 @@ void addNode()
             cout << "\033[31mDuplicate roll numbers allowed\033[0m" << endl;
             return;
         }
-        // if the list is empty, make the new node the START
-        newNode->next = START; // step 3: make the new node point to the first node
+        newNode->next = START; 
         if (START != NULL)
         {
-            START->prev = newNode; // step 4: maek the first node point to the new node
+            START->prev = newNode; 
         }
-        newNode->prev = NULL; // step 5: make the new node point to NULL
-        START = newNode;      // step 6: make the new node the first node
+        newNode->prev = NULL; 
+        START = newNode;      
     }
+    else
+    {
+      
+        Node* current = START; 
+        Node* previous = NULL;
+        while (current != NULL && current->noMhs < newNode->noMhs)
+        {                            
+            previous = current;     
+            current = current->next; 
+        }
